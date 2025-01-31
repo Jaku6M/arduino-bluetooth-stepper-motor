5f413eb265e1dede6971efdd3813d91bc39822a9
Jaku6M <j.mazurkiewicz13@wp.pl>
2025-01-31 19:45:18 +0100

    First try of a bluetooth code (not working right now)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,34 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+// software serial #1: TX = digital pin 23, RX = digital pin 22
+SoftwareSerial XM15B(23,22);
+char data;
 
-void setup() {
+void setup()
+{
+ // Open serial communications and wait for port to open:
+  //Serial.begin(9600);
 
+  // Start each software serial port
+  XM15B.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
 
+  // while there is data coming in, read it
+  // and send to the hardware serial port:
+  if (XM15B.available()) 
+  {
+    data = XM15B.read();
+    if (data == 'L')
+    {
+      digitalWrite(LED_BUILTIN,1);
+    }
+    if (data == 'l')
+    {
+      digitalWrite(LED_BUILTIN,0);
+    }
+  }
+}
